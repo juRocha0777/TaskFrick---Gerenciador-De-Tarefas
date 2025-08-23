e154494682adcf28ba55618d596b093f50af91a9
juRocha0777 <joaosilva@email.com>
2025-08-23 16:47:36 -0300

    fix: config do Cors Backend

---
+++ TickFlick -Aplication/Backend/TaskFriP/Gerenciador-Tarefas/gerenciadortarefas/src/main/java/com/junior/gerenciadortarefas/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.junior.gerenciadortarefas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")  // durante dev é ok
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
